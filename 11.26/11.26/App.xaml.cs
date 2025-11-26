19bc1e0549057e1971feaac7ca62bcccc6145194
Programozó <programozo@taszi.lan>
2025-11-26 13:44:59 +0100

    Button és textbox létrehozva.

---
+++ 11.26/11.26/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _11._26_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
